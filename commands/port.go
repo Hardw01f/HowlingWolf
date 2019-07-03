92d51befb471099c89a3563708f60a920dd59b97
RYO Tsukayama <e155728@ie.u-ryukyu.ac.jp>
2019-07-03 16:15:00 +0900

    Replace rapid portscan method

--- commands/port.go
+++ commands/port.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"runtime"
 	"strconv"
+	"sync"
 	"time"
 
 	portscanner "github.com/anvie/port-scanner"
@@ -19,6 +22,8 @@ type PortCmd struct {
 	Scan    bool
 	Monitor bool
 }
+
+var Dist string = "localhost"
 
 func (*PortCmd) Name() string     { return "port" }
 func (*PortCmd) Synopsis() string { return "Port Handling" }
@@ -39,35 +44,64 @@ func (p *PortCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) 
 	if p.Test {
 		fmt.Println("MS-06S")
 	} else if p.Scan {
-		//fmt.Println(flag.NArg())
-		if flag.NArg() != 4 {
-			fmt.Println("Usage : port -scan [Start-port] [End-port]")
-			os.Exit(1)
+			if flag.NArg() != 4 {
+				fmt.Println("Usage : port -scan [Start-port] [End-port]")
+				os.Exit(1)
+			}
+
+			StartPort, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			EndPort, err := strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		wg := new(sync.WaitGroup)
+		cpus := runtime.NumCPU()
+		fmt.Printf("runtime.CPUS : %d\n", cpus)
+		runtime.GOMAXPROCS(cpus)
+
+		var semaphoMultiple int
+		if cpus == 2 {
+			semaphoMultiple = 150
+		} else {
+			semaphoMultiple = 300
 		}
-		fmt.Println("Scan Starting")
-		//fmt.Println(args)
+		fmt.Printf("semaphoMultiple : %d\n", semaphoMultiple)
 
-		ps := portscanner.NewPortScanner("localhost", 2*time.Second, 5)
+		semapho := make(chan int, cpus*semaphoMultiple)
 
-		fmt.Printf("scanning port %s-%s...\n", args[0], args[1])
+		for {
+			logfile, err := os.OpenFile("portscan.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer logfile.Close()
 
-		StartPort, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println(err)
+			jpn, err := time.LoadLocation("Asia/Tokyo")
+			nowtime := time.Now().In(jpn)
+			fmt.Fprintf(logfile, " --- %s --- \n", nowtime)
+
+			for PortNum := StartPort; PortNum <= EndPort; PortNum++ {
+				//fmt.Printf("Goroutine : %d\n", runtime.NumGoroutine())
+				//fmt.Println(PortNum)
+				/*if PortNum == Maxnum {
+						fmt.Println("last port started")
+				}*/
+
+				wg.Add(1)
+				go Scan(PortNum, semapho, wg, logfile)
+			}
+			wg.Wait()
+			fmt.Println("finish")
+			fmt.Fprintln(logfile, "")
+			//DebugScan(5060)
+			time.Sleep(180 * time.Second)
 		}
 
-		EndPort, err := strconv.Atoi(args[1])
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		openedPorts := ps.GetOpenedPort(StartPort, EndPort)
-
-		for i := 0; i < len(openedPorts); i++ {
-			port := openedPorts[i]
-			fmt.Print(" ", port, " [open]")
-			fmt.Println("  -->  ", ps.DescribePort(port))
-		}
 	} else if p.Monitor {
 		if flag.NArg() != 3 {
 			fmt.Println("Usage : port -monitor [MonitorPortNumber]")
@@ -99,14 +133,14 @@ func CheckPort(Port string) {
 	for {
 		openport = ps.GetOpenedPort(OnePort, OnePort)
 		if len(openport) == 0 {
-			WallNotification(TargetPort,TargetPortProcess)
+			WallNotification(TargetPort, TargetPortProcess)
 			os.Exit(1)
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func WallNotification(TargetPort int,TargetPortProcess string) {
+func WallNotification(TargetPort int, TargetPortProcess string) {
 	loc, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
 		log.Fatal(err)
@@ -121,4 +155,25 @@ func WallNotification(TargetPort int,TargetPortProcess string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Scan(PortNum int, semapho chan int, wg *sync.WaitGroup, file *os.File) {
+	semapho <- 1
+
+	_, err := net.DialTimeout("tcp", Dist+":"+strconv.Itoa(PortNum), (1500 * time.Millisecond))
+
+	if err != nil {
+		/*fmt.Printf(" Checking %d Port\n",PortNum)
+		if PortNum%100 ==0{
+				fmt.Printf("Goroutine num is       %d\n",runtime.NumGoroutine())
+		}*/
+
+	} else {
+		fmt.Fprintf(file, "%d Port Opened\n", PortNum)
+		fmt.Printf("%d  Port Opened\n", PortNum)
+		//fmt.Printf("Goroutine : %d\n", runtime.NumGoroutine())
+
+	}
+	<-semapho
+	defer wg.Done()
 }
